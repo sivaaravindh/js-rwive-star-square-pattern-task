ee3217737b9504f65cf85d96a1b5789aa7382e53
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-26 19:18:51 +0530

    index

---
+++ main.js
@@ -0,0 +1,41 @@
+
+// let num= 5; 
+// let str = "";
+// for(let i = 0; i < num; i++) { 
+//   for(let j = 0; j < num; j++) { 
+//     str += "*";
+//   }
+  
+//   str += "\n";
+// }
+// console.log(str);
+
+
+
+// let count=0
+// let i,j 
+// for(i=2;i<=num1;i++)
+// {
+// for( j=1;j<=i;j++)
+// {
+//   if(i%j==0)
+//   count++  
+// }
+// console.log(i) 
+// }
+// let num1 =parseInt (prompt("Enter the number"));
+
+// for(var i=2 ; i <= num1; i++){
+
+//     for(var j=2;j<=i;j++){
+//         if(i% j==0){
+//              break;
+//         }
+//     }
+    
+//     if(i==j){
+//         document.write(i+"<br>")
+//     }
+// }
+
+
